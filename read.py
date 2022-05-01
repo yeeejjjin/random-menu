980775cc2e114c478d2185e5af26bf78a54679c8
yeeejjjin <yeeejjjin@gmail.com>
2022-05-01 20:27:12 +0900

    feat(#3): read file & split

--- read.py
+++ read.py
@@ -1,5 +1,3 @@
-import random
-
 file = open("/Users/joyejin/project/random-menu/data.txt", "r")
 
 strings = file.readlines()
@@ -23,14 +21,3 @@ for s in strings:
     menus.append({"name": arr[0], "price": arr[1], "category": arr[2]})
 
 print(menus)
-
-
-print("메뉴:")
-for menu in menus:
-    print(menu["name"], menu["price"], menu["category"])
-
-i = random.randint(0, len(menus) - 1)
-print("\n\n[추천]")
-print("- 이름:", menus[i]["name"])
-print("- 가격:", menus[i]["price"])
-print("- 카테고리:", menus[i]["category"])
