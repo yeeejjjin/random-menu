7cf7690c5e11bf2adc072f203ec7330d265125e1
yeeejjjin <yeeejjjin@gmail.com>
2022-05-01 20:33:22 +0900

    feat(#3): 필요없는거 삭제

--- lunch.py
+++ lunch.py
@@ -5,24 +5,12 @@ file = open("/Users/joyejin/project/random-menu/data.txt", "r")
 strings = file.readlines()
 file.close()
 
-print(strings)
-print("==========")
-
 menus = []
 
 for s in strings:
     s = s.replace("\n", "")
-    print(s)  # 떡볶이,1000,분식
-
     arr = s.split(",")
-    print(arr)  # ['떡볶이', '1000', '분식']
-    print(arr[0])
-    print(arr[1])
-    print(arr[2])
-
     menus.append({"name": arr[0], "price": arr[1], "category": arr[2]})
-
-print(menus)
 
 
 print("메뉴:")
